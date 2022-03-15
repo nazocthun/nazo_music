72afebf9b17836e233f9029ee744301bc8376a81
nazocthun <nazocthun@gmail.com>
2022-03-15 20:06:36 +0800

    more interfaces

--- src/utils/interfaces.ts
+++ src/utils/interfaces.ts
@@ -1,12 +1,27 @@
 // 解析音乐 接口和方法
 
-export interface albumInfo {
+export interface albumInfoTypes {
+  name: string,
+  size: number,
   picUrl: string,
   info: {
     commentCount: number,
   }
   publishTime: number | string,
   description: String,
+}
+
+export interface albumTopInfoTypes {
+  picUrl: string,
+  name: string,
+  musicSize: number,
+  albumSize: number,
+}
+
+export interface similarArtistTypes {
+  id: number,
+  name: string,
+  picUrl: string,
 }
 
 interface song {

9a43ec2a0fb6c276c6ebebfd4a822577435dee00
nazocthun <nazocthun@gmail.com>
2022-03-24 13:19:53 +0800

    element auto import

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const AutoImport = require('unplugin-auto-import/webpack')
+const ElementPlus = require('unplugin-element-plus/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 
@@ -7,14 +8,22 @@ module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
+      ElementPlus({
+        importStyle: 'sass',
+        useSource: true
+      }),
       AutoImport({
-        resolvers: [ElementPlusResolver({ exclude: new RegExp(/^(?!.*loading-directive).*$/) })],
-        // Element Plus bug, https://github.com/element-plus/element-plus/issues/4855
-        // temporary solution: https://blog.csdn.net/weixin_43245095/article/details/123105706
+        resolvers: [
+          ElementPlusResolver({
+            exclude: new RegExp(/^(?!.*loading-directive).*$/),
+            // Element Plus bug, https://github.com/element-plus/element-plus/issues/4855
+            // temporary solution: https://blog.csdn.net/weixin_43245095/article/details/123105706
+          })
+        ],
       }),
       Components({
         resolvers: [ElementPlusResolver()],
-        dts: true,
+        // dts: true,
       }),
     ],
   },

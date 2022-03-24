2b5387292f81bc8d3d1712a23733a5480539f5f1
nazocthun <nazocthun@gmail.com>
2022-03-24 13:21:09 +0800

    temp time format function, will be removed when new api is done

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,18 @@
 // 公用函数
-export function durationToTimeStamps(duration: number): string {
+export function durationTimeFormat(duration: number): string {
   let min = Math.floor(duration / 60000)
   let second = Math.floor((duration % 60000) / 1000)
   return min.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0')
+}
+
+export function playControlTimeFormat(duration: number | undefined): string {
+  if (typeof(duration) === 'undefined') {
+    return '00:00'
+  } else {
+    let min = Math.floor(duration / 60)
+    let second = Math.floor(duration % 60)
+    return min.toString().padStart(2, '0') + ':' + second.toString().padStart(2, '0')
+  }
 }
 
 export  function formatDate(now: Date): string { 

6f65dc0e64aaf9d2197b2971a52f1c7eaa4bcaeb
nazocthun <nazocthun@gmail.com>
2022-03-27 19:57:27 +0800

    add svg-sprite-loader

---
+++ src/components/SvgIcon/SvgIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <svg class="svg-icon">
+    <use :xlink:href="`#${iconName}`" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+  const props = defineProps({
+    iconName: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+
+<style scoped>
+.svg-icon {
+  @apply fill-current overflow-hidden w-full h-full
+}
+</style>
